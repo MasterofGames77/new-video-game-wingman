4d1a19749a89b938e88b782536d1d14a266f0026
MasterofGames77 <mgambardella16@gmail.com>
2024-12-04 18:57:52 -0500

    updated next.config file

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,24 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
+  images: {
+    domains: ['videogamewingman.com'],
+  },
   env: {
     NEXT_PUBLIC_TWITCH_CLIENT_ID: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID || '',
     TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET || '',
     TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI || '',
     RAWG_API_KEY: process.env.RAWG_API_KEY || '',
-    MONGODB_URI: process.env.MONGODB_URI || '', // Ensure MongoDB URI is included
+    MONGODB_URI: process.env.MONGODB_URI || '',
+    NEXT_PUBLIC_DOMAIN: 'https://videogamewingman.com',
   },
 };
 
